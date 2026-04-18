cbbfc43576c899cb0b193550965cee8a3373d306
Kiyara <IT23217768@my.sliit.lk>
2026-04-18 18:26:51 +0530

    Phase 02: Addition of repositories to the backend of the Ticket System. Backend updated.

---
+++ backend/src/main/java/com/smartcampus/backend/repository/TicketAttachmentRepository.java
@@ -0,0 +1,9 @@
+package com.smartcampus.backend.repository;
+
+import com.smartcampus.backend.model.TicketAttachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TicketAttachmentRepository extends JpaRepository<TicketAttachment, Long> {
+    List<TicketAttachment> findByTicketId(Long ticketId);
+}
